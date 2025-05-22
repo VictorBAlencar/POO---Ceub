d6018ca9cea3a541a157998d6a4c326fc1f1c90b
Victor Borges de Alencar <victor.borges.070@dinfor.uniceub>
2025-05-22 10:12:50 -0300

    Continuação de Banco de Dados c/ Java

---
+++ Aula_13_Banco_2/src/apresentacao/ControladorGravar.java
@@ -0,0 +1,40 @@
+package apresentacao;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import negocio.Autor;
+import negocio.Livro;
+
+public class ControladorGravar implements ActionListener {
+	
+	private JTextField txtTitulo = null;
+	private JComboBox<String> cboAutor = null;
+	
+	//Construtor Cheio
+	
+	public ControladorGravar(JTextField txtTitulo, JComboBox<String> cboAutor) {
+		super();
+		this.txtTitulo = txtTitulo;
+		this.cboAutor = cboAutor;
+	}
+
+	
+	public void actionPerformed(ActionEvent e) {
+		Livro objLivro = new Livro();
+		
+		objLivro.setTitulo(txtTitulo.getText());
+		objLivro.setObjAutor(new Autor(cboAutor.getSelectedIndex(), ""));
+	
+		try {
+			objLivro.persistir();
+			JOptionPane.showMessageDialog(null, "Gravação realizada com sucesso.");
+		}catch (Exception erro){
+			JOptionPane.showMessageDialog(null, erro);
+		}
+	}
+}
